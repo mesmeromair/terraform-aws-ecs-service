a5efcac15ab24880b1899752cb7499445c0647d6
mesmeromair <36470484+mesmeromair@users.noreply.github.com>
2022-01-28 04:32:19 +0500

    Remove port dependency from service_registries

--- main.tf
+++ main.tf
@@ -544,7 +544,6 @@ resource "aws_ecs_service" "main" {
       registry_arn   = service_registries.value.registry_arn
       container_name = service_registries.value.container_name
       container_port = service_registries.value.container_port
-      port           = service_registries.value.port
     }
   }
 }
